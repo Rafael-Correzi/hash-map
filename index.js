fadd19b3c95d4582b3ceed6db612ac236c59b55b
Rafael-Correzi <112991986+Rafael-Correzi@users.noreply.github.com>
2025-02-23 15:53:59 -0300

    Add way to count nodes in linked list

--- index.js
+++ index.js
@@ -1,25 +1,40 @@
 class HashMap {
   constructor() {
-    
+    this.arr = [];
+    this.createLinkedLists();
   }
 
-  const LOADFACTOR = 0.8;
-  let capacity = 16;
+  LOADFACTOR = 0.8;
+  capacity = 16;
 
-  function hash(key) {
+  createLinkedLists() {
+    for (let i = 0; i < this.capacity; i++) {
+      this.arr[i] = new LinkedList;
+    }
+  }
+
+  hash(key) {
     let hashCode = 0;
        
     const primeNumber = 31;
     for (let i = 0; i < key.length; i++) {
-      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % capacity;
+      hashCode = (primeNumber * hashCode + key.charCodeAt(i)) % this.capacity;
     }
  
     return hashCode;
     
   } 
 
-  function set(key, value) {
-
+  set(key, value) {
+    let hashCode = this.hash(key);
+    if (this.arr[hashCode] != undefined) {
+      console.log(this.arr)
+      if (this.arr[hashCode].contains(key)) {
+        this.arr[hashCode].key = value;
+      }
+      else this.arr[hashCode].append({key, value});
+    }
+    else this.arr[hashCode].append({key, value});
   }
 }
 
@@ -30,14 +45,34 @@ class LinkedList {
 
   head = null;
   tail = null;
+  nodeCount = 0;
 
   append(node) {
-    if (head === null) {
+    if (this.head === null) {
       this.head = new Node(node);
+      this.tail = this.head;
     }
     else {
-      this.tail = new Node(node);
+      let temp = new Node(node);
+      this.tail.nextNode = temp;
+      this.tail = temp;   
     }
+    this.nodeCount++;
+  }
+
+  contains(key) {
+    let current = this.head;
+    do {
+      if (key in current) {
+        return true;
+      }
+      current = current.nextNode;
+    } while (current != null);
+    return false;
+  }
+
+  get size() {
+    return this.nodeCount;
   }
 }
 
@@ -46,3 +81,8 @@ class Node {
     this.node = node;
   }
 }
+
+let a = new HashMap();
+a.set("luke", 2001);
+a.set("luke", 2005);
+console.log(a);
